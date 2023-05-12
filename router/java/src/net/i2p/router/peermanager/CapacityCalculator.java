f33ac6c89aa8d05faf87ac244c2799e6fefbace1
idk <hankhill19580@gmail.com>
2023-05-12 17:43:42 +0000

    start implementing congestion caps handling by adding CAPABILITY_CONGESTION_SEVER to TunnelPeerSelector.DEFAULT_EXCLUDE_CAPS

--- router/java/src/net/i2p/router/peermanager/CapacityCalculator.java
+++ router/java/src/net/i2p/router/peermanager/CapacityCalculator.java
@@ -34,6 +34,9 @@ class CapacityCalculator {
     private static final double PENALTY_NO_R_CAP = 1;
     private static final double PENALTY_U_CAP = 2;
     private static final double PENALTY_LAST_SEND_FAIL = 4;
+    private static final double PENALTY_D_CAP = 1;
+    private static final double PENALTY_E_CAP = 2;
+    //private static final double PENALTY_G_CAP = 4;
     private static final double PENALTY_RECENT_SEND_FAIL = 4;
     private static final double BONUS_LAST_SEND_SUCCESS = 1;
     private static final double BONUS_RECENT_SEND_SUCCESS = 1;
@@ -124,11 +127,13 @@ class CapacityCalculator {
                     capacity -= PENALTY_U_CAP;
                 if (caps.indexOf(Router.CAPABILITY_BW32) >= 0)
                     capacity -= PENALTY_L_CAP;
-/* TODO
+/* TODO */
                 if (caps.indexOf(Router.CAPABILITY_CONGESTION_MODERATE) >= 0)
                     capacity -= PENALTY_D_CAP;
                 else if (caps.indexOf(Router.CAPABILITY_CONGESTION_SEVERE) >= 0)
                     capacity -= PENALTY_E_CAP;
+/* TODO: G caps can be excluded in TunnelPeerSelector by adding it to DEFAULT_EXCLUDE_CAPS
+                decide what other handling if any is needed here.
                 else if (caps.indexOf(Router.CAPABILITY_NO_TUNNELS) >= 0)
                     capacity -= PENALTY_G_CAP;
 */
