8e2d99cfee08f118410cd1f90321beeb8df879cc
eyedeekay <idk@mulder>
2023-07-18 14:20:15 -0400

    Try some possible penalties for the congestion caps in the capacity calculator

--- router/java/src/net/i2p/router/peermanager/CapacityCalculator.java
+++ router/java/src/net/i2p/router/peermanager/CapacityCalculator.java
@@ -35,9 +35,11 @@ class CapacityCalculator {
     private static final double PENALTY_U_CAP = 2;
     private static final double PENALTY_LAST_SEND_FAIL = 4;
     // congestion cap penalty not yet applied
-    //private static final double PENALTY_D_CAP = 0;
+    private static final String PROP_D_CAP = "router.dcapPenalty";
+    private static final double PENALTY_D_CAP = 0.5;
     // congestion cap penalty not yet applied
-    //private static final double PENALTY_E_CAP = 0;
+    private static final String PROP_E_CAP = "router.ecapPenalty";
+    private static final double PENALTY_E_CAP = 0.9;
     //private static final double PENALTY_G_CAP = 0;
     private static final double PENALTY_RECENT_SEND_FAIL = 4;
     private static final double BONUS_LAST_SEND_SUCCESS = 1;
@@ -129,16 +131,27 @@ class CapacityCalculator {
                     capacity -= PENALTY_U_CAP;
                 if (caps.indexOf(Router.CAPABILITY_BW32) >= 0)
                     capacity -= PENALTY_L_CAP;
-/* TODO */
-                /*if (caps.indexOf(Router.CAPABILITY_CONGESTION_MODERATE) >= 0)
-                    capacity -= PENALTY_D_CAP;
-                else if (caps.indexOf(Router.CAPABILITY_CONGESTION_SEVERE) >= 0)
-                    capacity -= PENALTY_E_CAP;*/
-/* TODO: G caps can be excluded in TunnelPeerSelector by adding it to DEFAULT_EXCLUDE_CAPS
-                decide what other handling if any is needed here.
-                else if (caps.indexOf(Router.CAPABILITY_NO_TUNNELS) >= 0)
-                    capacity -= PENALTY_G_CAP;
-*/
+                if (caps.indexOf(Router.CAPABILITY_CONGESTION_MODERATE) >= 0) {
+                    String dcapPenalty = context.getProperty(PROP_D_CAP);
+                    if (dcapPenalty != null) {
+                        double dcap = Double.parseDouble(dcapPenalty);
+                        capacity -= dcap;
+                    } else {
+                        capacity -= PENALTY_D_CAP;
+                    }
+                } else if (caps.indexOf(Router.CAPABILITY_CONGESTION_SEVERE) >= 0){
+                    String ecapPenalty = context.getProperty(PROP_E_CAP);
+                    if (ecapPenalty != null) {
+                        double ecap = Double.parseDouble(ecapPenalty);
+                        capacity -= ecap;
+                    } else {
+                        capacity -= PENALTY_E_CAP;
+                    }
+                }
+                /* TODO: G caps can be excluded in TunnelPeerSelector by adding it to DEFAULT_EXCLUDE_CAPS */
+                // decide what other handling if any is needed here.
+                //else if (caps.indexOf(Router.CAPABILITY_NO_TUNNELS) >= 0)
+                //    capacity -= PENALTY_G_CAP;
             } else {
                 capacity -= PENALTY_NO_RI;
             }
