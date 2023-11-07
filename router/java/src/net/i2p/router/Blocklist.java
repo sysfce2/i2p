b91fe37fed079aa11554045fbba2a3b7b37825d9
eyedeekay <idk@mulder>
2023-11-07 14:27:59 -0500

    Router: Revert blocklist changes

--- router/java/src/net/i2p/router/Blocklist.java
+++ router/java/src/net/i2p/router/Blocklist.java
@@ -227,9 +227,11 @@ public class Blocklist {
         // but it's important to have this initialized before we read in the netdb.
         //job.getTiming().setStartAfter(_context.clock().now() + 30*1000);
         _context.jobQueue().addJob(job);
-        Job cleanupJob = new CleanupJob();
-        cleanupJob.getTiming().setStartAfter(_context.clock().now() + expireInterval());
-        _context.jobQueue().addJob(cleanupJob);
+        if (expireInterval() > 0) {
+            Job cleanupJob = new CleanupJob();
+            cleanupJob.getTiming().setStartAfter(_context.clock().now() + expireInterval());
+            _context.jobQueue().addJob(cleanupJob);
+        }
     }
 
     /**
@@ -278,21 +280,12 @@ public class Blocklist {
             return "Expire blocklist at user-defined interval of " + expireInterval();
         }
         public void runJob() {
-            int jobInterval;
-
-            if (expireInterval() > 0) {
-                clear();
-                _lastExpired = System.currentTimeMillis();
-                jobInterval = expireInterval();
-                if (_log.shouldLog(Log.DEBUG))
-                    _log.debug("Expiring blocklist entrys at" + _lastExpired);
-            } else {
-                // Set the next job interval to 15 minutes when expireInterval disabled
-                jobInterval = 15 * 60 * 1000;
-            }
-
+            clear();
+            _lastExpired = System.currentTimeMillis();
+            if (_log.shouldLog(Log.DEBUG))
+                _log.debug("Expiring blocklist entrys at" + _lastExpired);
             // schedule the next one
-            super.requeue(jobInterval);
+            super.requeue(expireInterval());
         }
     }
 
