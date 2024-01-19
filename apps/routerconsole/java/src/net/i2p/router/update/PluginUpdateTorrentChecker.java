25ba8926b74ddbfca371d7d082f18dcb98d32e30
eyedeekay <idki2p@i2pmail.org>
2024-01-19 12:52:57 -0500

    Plugins: fix flow for http-update-to-torrent-upgrade, torrent-failed-to-http-fallback

--- apps/routerconsole/java/src/net/i2p/router/update/PluginUpdateTorrentChecker.java
+++ apps/routerconsole/java/src/net/i2p/router/update/PluginUpdateTorrentChecker.java
@@ -9,17 +9,19 @@ import net.i2p.router.RouterContext;
 import net.i2p.router.web.ConfigUpdateHandler;
 import net.i2p.update.UpdateMethod;
 import net.i2p.update.UpdateType;
+import net.i2p.util.EepGet;
 import net.i2p.util.PartialEepGet;
 import net.i2p.util.PortMapper;
 
-public class PluginUpdateTorrentChecker extends UpdateRunner {
+public class PluginUpdateTorrentChecker extends PluginUpdateChecker {
     private final String _appName;
     private final String _oldVersion;
     private final URI _torrentURI;
 
     public PluginUpdateTorrentChecker(RouterContext ctx, ConsoleUpdateManager mgr,
-                               List<URI> uris, String appName, String oldVersion ) { 
-        super(ctx, mgr, UpdateType.PLUGIN, uris, oldVersion);
+            List<URI> uris, String appName, String oldVersion) {
+        //super(ctx, mgr, UpdateType.PLUGIN, uris, oldVersion);
+        super(ctx, mgr, uris, appName, oldVersion);
         if (!uris.isEmpty())
             _currentURI = uris.get(0);
         if (uris.size() == 2)
@@ -29,21 +31,24 @@ public class PluginUpdateTorrentChecker extends UpdateRunner {
         _appName = appName;
         _oldVersion = oldVersion;
     }
-    
-    @Override
-    public String getID() { return _appName; }
 
     @Override
     public void update() {
         boolean httpSeed = updateCheck(_currentURI.toString(), false);
+        // use the HTTP-only check to determine if there's an update available using
+        // PartialEepGet
         if (httpSeed) {
+            // Here we know there is an update, but we don't know if there's a torrent update
             boolean torrentSeed = updateCheck(_torrentURI.toString(), true);
             if (!torrentSeed) {
-                _mgr.notifyCheckComplete(this, false, false);
+                // No torrent update available, just use the HTTP update
+                super.update();
             } else {
-                
+                // There's a torrent update available
+                _mgr.notifyCheckComplete(this, true, true);
             }
         } else {
+            // HTTP-only update check failed, we know nothing(special)
             _mgr.notifyCheckComplete(this, false, false);
         }
     }
@@ -54,11 +59,12 @@ public class PluginUpdateTorrentChecker extends UpdateRunner {
         _isPartial = torrent;
         // performs exactly the same update check as PluginUpdateChecker
         // but returns a boolean value
-        String proxyHost = _context.getProperty(ConfigUpdateHandler.PROP_PROXY_HOST, ConfigUpdateHandler.DEFAULT_PROXY_HOST);
+        String proxyHost = _context.getProperty(ConfigUpdateHandler.PROP_PROXY_HOST,
+                ConfigUpdateHandler.DEFAULT_PROXY_HOST);
         int proxyPort = ConfigUpdateHandler.proxyPort(_context);
         if (proxyPort == ConfigUpdateHandler.DEFAULT_PROXY_PORT_INT &&
-            proxyHost.equals(ConfigUpdateHandler.DEFAULT_PROXY_HOST) &&
-            _context.portMapper().getPort(PortMapper.SVC_HTTP_PROXY) < 0) {
+                proxyHost.equals(ConfigUpdateHandler.DEFAULT_PROXY_HOST) &&
+                _context.portMapper().getPort(PortMapper.SVC_HTTP_PROXY) < 0) {
             String msg = _t("HTTP client proxy tunnel must be running");
             if (_log.shouldWarn())
                 _log.warn(msg);
@@ -68,7 +74,12 @@ public class PluginUpdateTorrentChecker extends UpdateRunner {
         updateStatus("<b>" + _t("Checking for update of plugin {0}", _appName) + "</b>");
         _baos.reset();
         try {
-            _get = new PartialEepGet(_context, proxyHost, proxyPort, _baos, uriString, TrustedUpdate.HEADER_BYTES);
+            if (_isPartial)
+                _get = new PartialEepGet(_context, proxyHost, proxyPort, _baos, uriString, TrustedUpdate.HEADER_BYTES);
+            else
+                _get = new EepGet(_context, true, proxyHost, proxyPort,
+                0, 0, 0, null, _baos,
+                uriString, false, null, null);
             if (torrent)
                 _get.addStatusListener(this);
             return _get.fetch(CONNECT_TIMEOUT);
@@ -76,26 +87,5 @@ public class PluginUpdateTorrentChecker extends UpdateRunner {
             _log.error("Error checking update for plugin", t);
         }
         return false;
-    }
-
-    @Override
-    public void transferComplete(long alreadyTransferred, long bytesTransferred, long bytesRemaining,
-                                    String url, String outputFile, boolean notModified) {
-        super.transferComplete(alreadyTransferred, bytesTransferred, bytesRemaining,
-                                url, outputFile, notModified);
-        // super sets _newVersion if newer
-        boolean newer = _newVersion != null;
-        if (newer) {
-            _mgr.notifyVersionAvailable(this, _currentURI, UpdateType.PLUGIN, _appName, UpdateMethod.TORRENT,
-                                        _urls, _newVersion, _oldVersion);
-        }
-        _mgr.notifyCheckComplete(this, newer, true);
-    }
-
-    @Override
-    public void transferFailed(String url, long bytesTransferred, long bytesRemaining, int currentAttempt) {
-        File f = new File(_updateFile);
-        f.delete();
-        _mgr.notifyCheckComplete(this, false, false);
     }
 }
--- apps/routerconsole/java/src/net/i2p/router/update/PluginUpdateTorrentRunner.java
+++ apps/routerconsole/java/src/net/i2p/router/update/PluginUpdateTorrentRunner.java
@@ -32,7 +32,6 @@ import net.i2p.util.SecureFile;
 import net.i2p.util.SystemVersion;
 import net.i2p.util.VersionComparator;
 
-
 /**
  * Check for an updated version of a plugin.
  * A plugin is a standard .sud file with a 40-byte signature,
@@ -50,12 +49,14 @@ import net.i2p.util.VersionComparator;
  */
 class PluginUpdateTorrentRunner extends PluginUpdateRunner {
 
-    /*private String _appName;
-    private final String _appDisplayName;
-    private final String _oldVersion;
-    private final URI _uri;
-    private final String _xpi2pURL;
-    private boolean _updated;*/
+    /*
+     * private String _appName;
+     * private final String _appDisplayName;
+     * private final String _oldVersion;
+     * private final URI _uri;
+     * private final String _xpi2pURL;
+     * private boolean _updated;
+     */
     private String _errMsg = "";
 
     private static final String XPI2P = "app.xpi2p";
@@ -64,120 +65,113 @@ class PluginUpdateTorrentRunner extends PluginUpdateRunner {
     private static final String PROP_ALLOW_NEW_KEYS = "routerconsole.allowUntrustedPlugins";
 
     public PluginUpdateTorrentRunner(RouterContext ctx, ConsoleUpdateManager mgr, List<URI> uris,
-                              String appName, String oldVersion ) {
+            String appName, String oldVersion) {
         super(ctx, mgr, uris, appName, oldVersion);
     }
 
-        @Override
-        protected void update() {
+    @Override
+    protected void update() {
 
-            _updated = false;
-            if (_xpi2pURL.startsWith("file:") || _method == UpdateMethod.FILE) {
-                // strip off file:// or just file:
-                String xpi2pfile = _uri.getPath();
-                if(xpi2pfile == null || xpi2pfile.length() == 0) {
-                        statusDone("<b>" + _t("Bad URL {0}", _xpi2pURL) + "</b>");
-                } else {
-                    // copy the contents of from to _updateFile
-                    long alreadyTransferred = (new File(xpi2pfile)).getAbsoluteFile().length();
-                    if(FileUtil.copy((new File(xpi2pfile)).getAbsolutePath(), _updateFile, true, false)) {
-                        updateStatus("<b>" + _t("Attempting to install from file {0}", _xpi2pURL) + "</b>");
-                        transferComplete(alreadyTransferred, alreadyTransferred, 0L, _xpi2pURL, _updateFile, false);
-                    } else {
-                        statusDone("<b>" + _t("Failed to install from file {0}, copy failed.", _xpi2pURL) + "</b>");
-                    }
-                }
+        _updated = false;
+        if (_xpi2pURL.startsWith("file:") || _method == UpdateMethod.FILE) {
+            // strip off file:// or just file:
+            String xpi2pfile = _uri.getPath();
+            if (xpi2pfile == null || xpi2pfile.length() == 0) {
+                statusDone("<b>" + _t("Bad URL {0}", _xpi2pURL) + "</b>");
             } else {
-                // use the same settings as for updater
-                //boolean shouldProxy = _context.getProperty(ConfigUpdateHandler.PROP_SHOULD_PROXY, ConfigUpdateHandler.DEFAULT_SHOULD_PROXY);
-                // always proxy, or else FIXME
-                boolean shouldProxy = true;
-                String proxyHost = _context.getProperty(ConfigUpdateHandler.PROP_PROXY_HOST, ConfigUpdateHandler.DEFAULT_PROXY_HOST);
-                int proxyPort = ConfigUpdateHandler.proxyPort(_context);
-                if (shouldProxy && proxyPort == ConfigUpdateHandler.DEFAULT_PROXY_PORT_INT &&
+                // copy the contents of from to _updateFile
+                long alreadyTransferred = (new File(xpi2pfile)).getAbsoluteFile().length();
+                if (FileUtil.copy((new File(xpi2pfile)).getAbsolutePath(), _updateFile, true, false)) {
+                    updateStatus("<b>" + _t("Attempting to install from file {0}", _xpi2pURL) + "</b>");
+                    transferComplete(alreadyTransferred, alreadyTransferred, 0L, _xpi2pURL, _updateFile, false);
+                } else {
+                    statusDone("<b>" + _t("Failed to install from file {0}, copy failed.", _xpi2pURL) + "</b>");
+                }
+            }
+        } else {
+            // use the same settings as for updater
+            // boolean shouldProxy =
+            // _context.getProperty(ConfigUpdateHandler.PROP_SHOULD_PROXY,
+            // ConfigUpdateHandler.DEFAULT_SHOULD_PROXY);
+            // always proxy, or else FIXME
+            boolean shouldProxy = true;
+            String proxyHost = _context.getProperty(ConfigUpdateHandler.PROP_PROXY_HOST,
+                    ConfigUpdateHandler.DEFAULT_PROXY_HOST);
+            int proxyPort = ConfigUpdateHandler.proxyPort(_context);
+            if (shouldProxy && proxyPort == ConfigUpdateHandler.DEFAULT_PROXY_PORT_INT &&
                     proxyHost.equals(ConfigUpdateHandler.DEFAULT_PROXY_HOST) &&
                     _context.portMapper().getPort(PortMapper.SVC_HTTP_PROXY) < 0) {
-                    String msg = _t("HTTP client proxy tunnel must be running");
-                    if (_log.shouldWarn())
-                        _log.warn(msg);
-                    statusDone("<b>" + msg + "</b>");
-                    _mgr.notifyTaskFailed(this, msg, null);
-                    return;
-                }
-                updateStatus("<b>" + _t("Downloading plugin from {0}", _xpi2pURL) + "</b>");
-                try {
-                    if (shouldProxy)
-                        // 10 retries!!
-                        _get = new EepGet(_context, proxyHost, proxyPort, 10, _updateFile, _xpi2pURL, false);
-                    else
-                        _get = new EepGet(_context, 1, _updateFile, _xpi2pURL, false);
-                    _get.addStatusListener(PluginUpdateTorrentRunner.this);
-                    _get.fetch(CONNECT_TIMEOUT, -1, shouldProxy ? INACTIVITY_TIMEOUT : NOPROXY_INACTIVITY_TIMEOUT);
-                } catch (Throwable t) {
-                    _log.error("Error downloading plugin", t);
-                }
+                String msg = _t("HTTP client proxy tunnel must be running");
+                if (_log.shouldWarn())
+                    _log.warn(msg);
+                statusDone("<b>" + msg + "</b>");
+                _mgr.notifyTaskFailed(this, msg, null);
+                return;
             }
-            if (_updated) {
-                _mgr.notifyComplete(this, _newVersion, null);
-                _mgr.notifyComplete(this, _errMsg);
-            } else {
-                _mgr.notifyTaskFailed(this, _errMsg, null);
+            updateStatus("<b>" + _t("Downloading plugin from {0}", _xpi2pURL) + "</b>");
+            try {
+                if (shouldProxy)
+                    // 10 retries!!
+                    _get = new EepGet(_context, proxyHost, proxyPort, 10, _updateFile, _xpi2pURL, false);
+                else
+                    _get = new EepGet(_context, 1, _updateFile, _xpi2pURL, false);
+                _get.addStatusListener(PluginUpdateTorrentRunner.this);
+                _get.fetch(CONNECT_TIMEOUT, -1, shouldProxy ? INACTIVITY_TIMEOUT : NOPROXY_INACTIVITY_TIMEOUT);
+            } catch (Throwable t) {
+                _log.error("Error downloading plugin", t);
             }
         }
+        if (_updated) {
+            _mgr.notifyComplete(this, _newVersion, null);
+            _mgr.notifyComplete(this, _errMsg);
+        } else {
+            _mgr.notifyTaskFailed(this, _errMsg, null);
+        }
+    }
 
     /**
-     *  Overridden to change the "Updating I2P" text in super
-     *  @since 0.9.35
+     * Overridden to change the "Updating I2P" text in super
+     * 
+     * @since 0.9.35
      */
     @Override
-    public void bytesTransferred(long alreadyTransferred, int currentWrite, long bytesTransferred, long bytesRemaining, String url) {
+    public void bytesTransferred(long alreadyTransferred, int currentWrite, long bytesTransferred, long bytesRemaining,
+            String url) {
         long d = currentWrite + bytesTransferred;
         String status = "<b>" + _t("Downloading plugin") + ": " + _appDisplayName + "</b>";
         _mgr.notifyProgress(this, status, d, d + bytesRemaining);
     }
 
-        @Override
-        public void transferComplete(long alreadyTransferred, long bytesTransferred, long bytesRemaining, String url, String outputFile, boolean notModified) {
-            if (!(_xpi2pURL.startsWith("file:") || _method == UpdateMethod.FILE))
-                updateStatus("<b>" + _t("Plugin downloaded") + ": " + _appDisplayName + "</b>");
-            File f = new File(_updateFile);
-            File appDir = new SecureDirectory(_context.getConfigDir(), PLUGIN_DIR);
-            if ((!appDir.exists()) && (!appDir.mkdir())) {
-                f.delete();
-                statusDone("<b>" + _t("Cannot create plugin directory {0}", appDir.getAbsolutePath()) + "</b>");
-                return;
-            }
-            boolean isSU3;
-            try {
-                isSU3 = isSU3File(f);
-            } catch (IOException ioe) {
-                f.delete();
-                statusDone("<b>" + ioe + "</b>");
-                return;
-            }
-            if (isSU3)
-                processSU3(f, appDir, url);
-            else
-                processSUD(f, appDir, url);
-        }
-
-        /**
-         *  @param pubkey null OK for su3
-         *  @since 0.9.15
-         */
-
-        @Override
-        public void transferFailed(String url, long bytesTransferred, long bytesRemaining, int currentAttempt) {
-            File f = new File(_updateFile);
+    @Override
+    public void transferComplete(long alreadyTransferred, long bytesTransferred, long bytesRemaining, String url,
+            String outputFile, boolean notModified) {
+        if (!(_xpi2pURL.startsWith("file:") || _method == UpdateMethod.FILE))
+            updateStatus("<b>" + _t("Plugin downloaded") + ": " + _appDisplayName + "</b>");
+        File f = new File(_updateFile);
+        File appDir = new SecureDirectory(_context.getConfigDir(), PLUGIN_DIR);
+        if ((!appDir.exists()) && (!appDir.mkdir())) {
             f.delete();
-            statusDone("<b>" + _t("Failed to download plugin from {0}", url) + "</b>");
+            statusDone("<b>" + _t("Cannot create plugin directory {0}", appDir.getAbsolutePath()) + "</b>");
+            return;
         }
+        boolean isSU3;
+        try {
+            isSU3 = isSU3File(f);
+        } catch (IOException ioe) {
+            f.delete();
+            statusDone("<b>" + ioe + "</b>");
+            return;
+        }
+        if (isSU3)
+            processSU3(f, appDir, url);
+        else
+            processSUD(f, appDir, url);
+    }
 
-        private void statusDone(String msg) {
-            // if we fail, we will pass this back in notifyTaskFailed()
-            _errMsg = msg;
-            updateStatus(msg);
-        }
+    private void statusDone(String msg) {
+        // if we fail, we will pass this back in notifyTaskFailed()
+        _errMsg = msg;
+        updateStatus(msg);
+    }
 
 }
-
